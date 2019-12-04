5a8ef04ac1a488d66aef98dcf4ba95f41d8e944c
ChanGrea <k39335@naver.com>
2019-12-04 09:01:22 +0900

    :construction: working Header

--- src/components/organisms/Header/index.js
+++ src/components/organisms/Header/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { Logo, Title, SearchBar } from 'components';
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
-  padding: 1.8rem;
+  padding: 0.5rem;
   background-color: yellowgreen;
 `;
 
 const Header = () => {
-  return <Wrapper />;
+  return (
+    <Wrapper>
+      <Logo />
+      <Title />
+      <SearchBar />
+    </Wrapper>
+  );
 };
 
 Header.propTypes = {};
