6660bfcd812927180e68f051eaaa348cb691194c
ChanGrea <k39335@naver.com>
2019-12-07 19:22:11 +0900

    :hammer: modified: SearchBar styles

--- src/components/molecules/SearchBar/index.js
+++ src/components/molecules/SearchBar/index.js
@@ -4,13 +4,25 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   margin: 0.5em;
-  float: left;
+  width: 100%;
 `;
 
 const SearchForm = styled.div`
-  width: 400px;
-  height: 30px;
-  background-color: white;
+  background-color: #fff;
+  border: 1px solid yellowgreen;
+  border-radius: 3px;
+  box-sizing: border-box;
+  height: 26px;
+  left: -1px;
+  margin-left: 30px;
+  margin-top: 0;
+  max-width: 448px;
+  overflow: hidden;
+  padding-left: 5px;
+  padding-right: 48px;
+  position: relative;
+  top: 8px;
+  width: auto;
 `;
 
 
