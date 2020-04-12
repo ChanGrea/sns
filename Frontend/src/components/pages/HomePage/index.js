0e38a1491270b6002981f1b0fcb3d04db7618e15
ChanGrea <k39335@naver.com>
2020-04-12 17:15:14 +0900

    :hammer: modified: main contents

--- Frontend/src/components/pages/HomePage/index.js
+++ Frontend/src/components/pages/HomePage/index.js
@@ -1,34 +1,36 @@
 import React from 'react';
 
 import { PageTemplate, Header, Footer } from 'components';
-import { Container, Row, Col, ListGroup, Tab } from 'react-bootstrap';
+import { Container, Row, Col, Nav, Tab } from 'react-bootstrap';
 
 const HomePage = () => {
   return (
     <PageTemplate header={<Header />} footer={<Footer />}>
-      <Container>
-        <Row>
-          <Col xs={3}>
-            <ListGroup>
-              <ListGroup.Item action href="#link1">
-                Link 1
-              </ListGroup.Item>
-              <ListGroup.Item action href="#link2">
-                Link 2
-              </ListGroup.Item>
-            </ListGroup>
-          </Col>
-          <Col>
-            <Tab.Content>
-              <Tab.Pane eventKey="#link1">
-                Link1 Contents
-              </Tab.Pane>
-              <Tab.Pane eventKey="#link2">
-                Link2 Contents
-              </Tab.Pane>
-            </Tab.Content>
-          </Col>
-        </Row>
+      <Container className="mt-4">
+        <Tab.Container id="main-left-tabs" defaultActiveKey="first">
+          <Row>
+            <Col sm={3}>
+              <Nav variant="pills" className="flex-column">
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Col>
+            <Col sm={9}>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  first tab contents
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  second tab contents
+                </Tab.Pane>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
       </Container>
     </PageTemplate>
   );
