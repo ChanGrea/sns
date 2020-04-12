c76574a5d681ea3f901cacb78262e20c08c9e54c
ChanGrea <k39335@naver.com>
2020-04-12 16:58:54 +0900

    :hammer: modified: main contents layout

--- Frontend/src/components/pages/HomePage/index.js
+++ Frontend/src/components/pages/HomePage/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
 
 import { PageTemplate, Header, Footer } from 'components';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, ListGroup, Tab } from 'react-bootstrap';
 
 const HomePage = () => {
   return (
     <PageTemplate header={<Header />} footer={<Footer />}>
       <Container>
         <Row>
-          <Col>sideMenu(left)</Col>
-          <Col xs={6}>Main Contents</Col>
-          <Col>sideMenu(right)</Col>
+          <Col xs={3}>
+            <ListGroup>
+              <ListGroup.Item action href="#link1">
+                Link 1
+              </ListGroup.Item>
+              <ListGroup.Item action href="#link2">
+                Link 2
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col>
+            <Tab.Content>
+              <Tab.Pane eventKey="#link1">
+                Link1 Contents
+              </Tab.Pane>
+              <Tab.Pane eventKey="#link2">
+                Link2 Contents
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
         </Row>
       </Container>
     </PageTemplate>
